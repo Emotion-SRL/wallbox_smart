65420afa0df427f188488fae8976960512b8e2ec
jackfioru92 <56474340+jackfioru92@users.noreply.github.com>
2024-04-26 14:32:10 +0200

    added django

--- src/main.py
+++ src/main.py
@@ -1,11 +1,37 @@
 import asyncio
-import websockets
-import requests
 import json
-import sys
+import os
+
+import requests
+import websockets
 
 connected_clients = {}
 status_requested = {}
+
+
+async def django(websocket, path):
+    try:
+        # Ricevere l'indirizzo IP del client
+        client_ip = websocket.remote_address[0]
+        print(f"Client connesso dall'indirizzo IP: {client_ip} sulla porta 15001")
+        # Memorizza l'indirizzo IP pubblico del client
+        connected_clients[client_ip] = websocket
+        # Attendere ulteriori messaggi dal client
+        async for message in websocket:
+            print(f"Ricevuto messaggio dal client {client_ip} sulla porta 15001: {message}")
+            # Qui puoi gestire il messaggio JSON come preferisci
+            message_data = json.loads(message)
+            if 'ip' in message_data:
+                target_ip = message_data['ip']
+                if target_ip in connected_clients:
+                    print(f"Invio messaggio al client con IP {target_ip}")
+                    # target_websocket = connected_clients[target_ip]
+                    # await target_websocket.send(json.dumps(message_data))
+                else:
+                    print(f"Client con IP {target_ip} non trovato.")
+
+    except websockets.exceptions.ConnectionClosedError:
+        print(f"Connessione chiusa dal client {client_ip} sulla porta 15001")
 
 
 async def server(websocket, path):
@@ -38,14 +64,14 @@ async def send_status(client_ip):
     # Verifica se il client è connesso
     if client_ip in connected_clients:
         # Invia il comando "status" al client specificato
-        await connected_clients[client_ip].send("set max amp 12")
+        print("Invio comando status al client")
     else:
         print(f"Client {client_ip} non trovato.")
 
 
 async def save_boot_notification_to_db(message):
     # URL dell'API per salvare i dati nel database
-    api_url = sys.env.get(
+    api_url = os.environ.get(
         'API_URL', "https://emotion-test.eu/api") + "/wallbox/"
     try:
         # Effettua la richiesta PATCH all'API
@@ -65,7 +91,7 @@ async def save_boot_notification_to_db(message):
 
 async def save_realtime_notification_to_db(message):
     # URL dell'API per salvare i dati nel database
-    api_url = sys.env.get(
+    api_url = os.environ.get(
         'API_URL', "https://emotion-test.eu/api") + "/wallbox/realtime/"
     try:
         # Effettua la richiesta PATCH all'API
@@ -83,6 +109,8 @@ async def save_realtime_notification_to_db(message):
         print(f"Errore durante la richiesta all'API: {e}")
 
 start_server = websockets.serve(server, "0.0.0.0", 8765)
+start_django = websockets.serve(django, "0.0.0.0", 15001)
+
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
